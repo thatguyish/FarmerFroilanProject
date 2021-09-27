faacedc224f68446155658062de708b7a77f9fc3
iGyalchester <borisgerard333@gmail.com>
2021-09-27 15:24:16 -0400

    Boris Commit

---
+++ src/main/java/mcfroilan/Chicken.java
@@ -0,0 +1,9 @@
+package mcfroilan;
+
+public class Chicken {
+
+	@Override
+	public void yield (boolean hasBeenFertilized){
+		System.out.println(Egg);
+	}
+}
---
+++ src/main/java/mcfroilan/Farmer.java
@@ -0,0 +1,6 @@
+package mcfroilan;
+
+public class Farmer {
+
+
+}
